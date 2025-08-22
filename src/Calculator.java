7881bfa164befb1d4b762a6b8c7afee8d95c710c
CharaBLk <bellourakcharaf@gmail.com>
2025-08-22 19:01:29 +0200

    Create a simple frame for a simple calculator ...

---
+++ src/Calculator.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+
+public class Calculator {
+    int boardWidth = 500;
+    int boardHeight = 540;
+
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
+    JFrame frame = new JFrame("Calculator");
+    JPanel displayPanel = new JPanel();
+    JLabel displayLabel = new JLabel();
+
+
+
+    Calculator(){
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.WHITE);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));     
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("00000000");
+        displayLabel.setOpaque(true);
+        
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel);
+        frame.setVisible(true);
+        
+    }
+
+}
