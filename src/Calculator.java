f64cf7a41cb80b38a3c8b2b43879050a41718143
CharaBLk <bellourakcharaf@gmail.com>
2025-08-24 13:28:07 +0200

    ...

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,13 @@
 import java.awt.*;
-import java.awt.event.*;;
+import java.awt.event.*;import java.lang.reflect.Array;
+;
 import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 
 public class Calculator {
-    int boardWidth = 500;
+    int boardWidth = 470;
     int boardHeight = 540;
 
     Color customLightGray = new Color(212, 212, 210);
@@ -32,6 +33,9 @@ public class Calculator {
     JPanel buttonsPanel = new JPanel();
     
 
+    String A = "0";
+    String operator = null;
+    String B = null;
 
     Calculator(){
         frame.setSize(boardWidth, boardHeight);
@@ -44,7 +48,7 @@ public class Calculator {
         displayLabel.setForeground(Color.WHITE);
         displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));     
         displayLabel.setHorizontalAlignment(JLabel.RIGHT);
-        displayLabel.setText("00000000");
+        displayLabel.setText("0");
         displayLabel.setOpaque(true);
         
         displayPanel.setLayout(new BorderLayout());
@@ -53,27 +57,79 @@ public class Calculator {
         
 
         buttonsPanel.setLayout(new GridLayout(5, 4));
-        buttonsPanel.setBackground(customOrange);
+        buttonsPanel.setBackground(customBlack);
         frame.add(buttonsPanel);
 
         for(int i = 0; i<ButtonValues.length; i++){
             
             JButton button = new JButton();
             String buttonValue = ButtonValues[i];
-            button.setFont(new Font("Arial", Font.PLAIN, 60));
+            button.setFont(new Font("Arial", Font.ITALIC, 60));
             button.setText(buttonValue);
+            button.setBackground(customDarkGray);
+            button.setForeground(Color.WHITE);
+            button.setFocusable(false);
+            
+
+            if(Arrays.asList(rightSymbols).contains(ButtonValues[i])){
+                button.setBackground(customOrange);
+                button.setForeground(Color.WHITE);
+            }
+            if(Arrays.asList(topSymbols).contains(ButtonValues[i])){
+                button.setBackground(customLightGray);
+                button.setForeground(customBlack);
+            }
             buttonsPanel.add(button);
             frame.add(buttonsPanel);
-            
+            button.addActionListener(new ActionListener() {
+                @SuppressWarnings("StringEquality")
+                public void actionPerformed(ActionEvent e){
+                    JButton button = (JButton) e.getSource();
+                    String buttonValue = button.getText();
+
+                    if(Arrays.asList(rightSymbols).contains(buttonValue)){
+
+                    }
+                    else if (Arrays.asList(topSymbols).contains(buttonValue)) {
+                        if(buttonValue=="AC"){
+                            clearAll();
+                            displayLabel.setText(A);
+                        }
+                        else if (buttonValue=="+/-") {
+                            double numDouble = Double.parseDouble(displayLabel.getText());
+                            
+                            numDouble *= -1;
+                            displayLabel.setText(Double.toString(numDouble));
+                        }
+                    }
+                    else{
+                        if(buttonValue == "."){
+                            if(!displayLabel.getText().contains(buttonValue)){
+                                displayLabel.setText(displayLabel.getText()+buttonValue);
+                            }
+                        }
+                        else if("0123456789".contains(buttonValue)){
+                            if(displayLabel.getText() == "0"){
+                                displayLabel.setText(buttonValue);
+                            }
+                            else{
+                                displayLabel.setText(displayLabel.getText()+buttonValue);
+                            }
+                        }
+                    }
+
+                }
+            });
         }
-
-
-
+        
         frame.setVisible(true);
-
         
     }
-
-    
-
+    public void clearAll(){
+        A="0";
+        operator = null;
+        B=null;
+    }
 }
+
+
